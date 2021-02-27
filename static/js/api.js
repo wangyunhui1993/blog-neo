cecd4805ac3ec6f3359d93c9ac74615226a88cd0
wangyunhui <1825548604@qq.com>
2021-02-27 16:45:27 +0800

    修改配置

--- static/js/api.js
+++ static/js/api.js
@@ -1,17 +1,21 @@
 // var urlBase = "http://neo123.cn:3000"
 // var urlBase = "http://localhost:3000"
-var urlBase = "https://localhost:3001"
-
+// var urlBase = "https://localhost:3001"
+var protocol = window.location.protocol;
+var hostname = window.location.hostname;
+var port = protocol === 'https:' ? '3001' : '3000'
+var urlBase = `${protocol}//${hostname}:${port}`
+console.log(urlBase);
 function request(url, info, callback) {
 	$.ajax({
 		type: "post",
 		url: urlBase + url,
 		dataType: "json",
 		data: info,
-		success: function(data) {
+		success: function (data) {
 			callback(data);
 		},
-		error: function(jqXHR) {
+		error: function (jqXHR) {
 			console.log("Error: " + jqXHR.status);
 			callback({
 				err_code: 10000,
