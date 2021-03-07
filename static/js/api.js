357ac830b6c1bf751567c5c5e37b355945708876
wangyunhui <1825548604@qq.com>
2021-03-07 22:36:25 +0800

    修改https端口号

--- static/js/api.js
+++ static/js/api.js
@@ -1,9 +1,7 @@
-// var urlBase = "http://neo123.cn:3000"
-// var urlBase = "http://localhost:3000"
-// var urlBase = "https://localhost:3001"
+
 var protocol = window.location.protocol;
 var hostname = window.location.hostname;
-var port = protocol === 'https:' ? '3001' : '3000'
+var port = protocol === 'https:' ? '444' : '3000'
 var urlBase = `${protocol}//${hostname}:${port}`
 console.log(urlBase);
 function request(url, info, callback) {
